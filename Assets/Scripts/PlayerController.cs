6f4a8bf5649d5857836455a4a23cb99157af9e68
02rasjac <rasmusjacklin2002@gmail.com>
2021-04-20 19:58:02 +0200

    Lecture #38: Rotate player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    /// <summary>var <c>boostSpeed</c> indicates the ammount of force to be applied in local y-axis</summary>
-    public int boostSpeed = 10;
+    /// <summary>var <c>boostSpeed</c> indicates the ammount of force to be applied in local y-axis. 
+    /// Default <value>1000</value></summary>
+    public int boostSpeed = 1000;
+
+    /// <summary>var <c>rotationSpeed</c> indicates the speed of rotation. 
+    /// Default <value>45f</value></summary>
+    public float rotationSpeed = 45f;
 
     Rigidbody rb;
 
@@ -22,17 +27,17 @@ public class PlayerController : MonoBehaviour
     /// <summary>method <c>Boos</c>Move player in direction of rotation</summary>
     void Boost() {
         if (Input.GetKey(KeyCode.Space)) {
-            rb.AddRelativeForce(Vector3.up * boostSpeed);
+            rb.AddRelativeForce(Vector3.up * boostSpeed * Time.deltaTime);
         }
     }
 
     /// <summary>method <c>Rotate</c>Rotate the player left/right</summary>
     void Rotate() {
         if (Input.GetKey(KeyCode.RightArrow)) {
-            Debug.Log("Rotating right");
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.LeftArrow)) {
-            Debug.Log("Rotating left");
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
         }
     }
 }
