93b28823b81dd9d851477279a574869885f4cd12
02rasjac <rasmusjacklin2002@gmail.com>
2021-04-20 19:30:59 +0200

    Lecture #36: Relative force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>var <c>boostSpeed</c> indicates the ammount of force to be applied in local y-axis</summary>
+    public int boostSpeed = 10;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         Boost();
         Rotate();
     }
 
-    /**
-     * Move player in direction of rotation
-     */
+    /// <summary>method <c>Boos</c>Move player in direction of rotation</summary>
     void Boost() {
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            Debug.Log("Rotating left");
+        if (Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(Vector3.up * boostSpeed);
         }
     }
 
-    /**
-     * Rotate the player left/right
-     */
+    /// <summary>method <c>Rotate</c>Rotate the player left/right</summary>
     void Rotate() {
         if (Input.GetKey(KeyCode.RightArrow)) {
             Debug.Log("Rotating right");
         }
-        if (Input.GetKey(KeyCode.Space)) {
-            Debug.Log("Boosting");
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Debug.Log("Rotating left");
         }
     }
 }
