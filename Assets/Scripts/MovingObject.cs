95842ab4a6c3f4930a523532545f1d2c57e32606
02rasjac <rasmusjacklin2002@gmail.com>
2021-04-26 20:36:07 +0200

    Lecture #57: Add moving obstacles

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public Vector3 moveVector;
+    [Tooltip("Lower == faster")]
+    public float period = 2f;
+
+    float moveFactor;
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Calculate the offset to move based on a sin-wave
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2;
+        float rawSin = Mathf.Sin(cycles * tau);
+        moveFactor = (rawSin + 1f) / 2;
+        Vector3 offset = moveVector * moveFactor;
+
+        // Set position
+        transform.position = startPosition + offset;
+    }
+}
